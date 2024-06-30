b5f1ebc6b3cd7fd977350f48d5573bb276f0180f
aslw1 <Abdelrahman.Eldahab@student.aast.edu>
2024-06-30 16:54:26 +0300

    b

--- app/signup/suppliersignup/Signup.tsx
+++ app/signup/suppliersignup/Signup.tsx
@@ -10,14 +10,27 @@ const Signup = () => {
     LogBox.ignoreLogs(['VirtualizedLists should never be nested']);
   }, []);
 
-  const [email, setEmail] = useState("");
-  const [password, setpassword] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setpassword] = useState('');
+  const [national_id, setNationalId] = useState('');
+  const [first_name, setFirstName] = useState('');
+  const [last_name, setLastName] = useState('');
+  const [telephone, setTelephone] = useState('');
+  const [commercial_id, setCommercialId] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
   const router = useRouter();
 
-  const onSubmit = async (email:string, password:string) => {
+  const onSubmit = async (
+    email: string,
+    password: string,
+    national_id: string,
+    first_name: string,
+    last_name: string,
+    telephone: string,
+    commercial_id: string
+  ) => {
     try {
-      const response = await fetch("http://localhost:3000/api/auth/owner/login", {
+      const response = await fetch("http://localhost:3000/api/auth/owner/signup", {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -79,55 +92,54 @@ const Signup = () => {
                 placeholder=""
                 value={password}
                 onChangeText={setpassword}
-                keyboardType="phone-pad" 
               />
               <Text style={styles.offerName} className='mt-5'>كلمه السر</Text>
               <TextInput
                 textContentType="password"
                 className='w-[350px] h-[50px] p-4 my-3 rounded-xl border-2 text-right border-[#c2c0c0]' 
-                placeholder=""
-                value={password}
-                onChangeText={setpassword}
-                keyboardType="phone-pad" 
+                placeholder="National ID"
+                value={national_id}
+                onChangeText={setNationalId}
+         
               />
               <Text style={styles.offerName} className='mt-5'>كلمه السر</Text>
               <TextInput
                 textContentType="password"
                 className='w-[350px] h-[50px] p-4 my-3 rounded-xl border-2 text-right border-[#c2c0c0]' 
-                placeholder=""
-                value={password}
-                onChangeText={setpassword}
-                keyboardType="phone-pad" 
+                placeholder="First Name"
+                value={first_name}
+                onChangeText={setFirstName}
+         
               />
               <Text style={styles.offerName} className='mt-5'>كلمه السر</Text>
               <TextInput
                 textContentType="password"
                 className='w-[350px] h-[50px] p-4 my-3 rounded-xl border-2 text-right border-[#c2c0c0]' 
-                placeholder=""
-                value={password}
-                onChangeText={setpassword}
-                keyboardType="phone-pad" 
+                placeholder="Last Name"
+                value={last_name}
+                onChangeText={setLastName}
+             
               />
               <Text style={styles.offerName} className='mt-5'>كلمه السر</Text>
               <TextInput
                 textContentType="password"
                 className='w-[350px] h-[50px] p-4 my-3 rounded-xl border-2 text-right border-[#c2c0c0]' 
-                placeholder=""
-                value={password}
-                onChangeText={setpassword}
-                keyboardType="phone-pad" 
+                value={telephone}
+                onChangeText={setTelephone}
+                keyboardType="phone-pad"
+              
               />
               <Text style={styles.offerName} className='mt-5'>كلمه السر</Text>
               <TextInput
                 textContentType="password"
                 className='w-[350px] h-[50px] p-4 my-3 rounded-xl border-2 text-right border-[#c2c0c0]' 
-                placeholder=""
-                value={password}
-                onChangeText={setpassword}
-                keyboardType="phone-pad" 
+                placeholder="Commercial ID"
+                value={commercial_id}
+                onChangeText={setCommercialId}
+                
               />
               <Text style={styles.offerName} className='mt-5 text-red-500'>{errorMessage}</Text>
-               <Button title="sign up" onPress={() => onSubmit(email, password)} />
+               <Button title="sign up" onPress={() => onSubmit(email, password,national_id,first_name,last_name,telephone,commercial_id)} />
               {/* <Link href={"/Supplier"} className='p-3 bg-[#69B056] rounded-3xl text-center mt-4'>
                 <Text className='text-white' style={styles.offerName}>تسجيل</Text>
               </Link> */}
